3a3b619be28a8234576d4d0a18bb4b0f4bd500ce
RobWeenden <weendenandrade@gmail.com>
2021-02-23 19:22:26 -0300

    Removing the dprecated FileUpload import class

--- src/servlet/UsuarioServlet.java
+++ src/servlet/UsuarioServlet.java
@@ -3,7 +3,6 @@ package servlet;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,8 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.tomcat.util.codec.binary.Base64;
 
